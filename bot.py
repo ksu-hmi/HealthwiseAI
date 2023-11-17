7fe953cd2657151d9f945347ef159dced0640508
Oghoreye  Obazee <142323270+Coletteobazee@users.noreply.github.com>
2023-11-17 09:00:39 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from openai import OpenAI
 
 # Set your OpenAI GPT-3 API key
-client = OpenAI(api_key=os.environ['TXHpyUizxQUVFWNOJAsWR6QK2JEzEngl79leJoMifNFDpa6D9tgoyeHOWZ7wcve9AQP0Sa5qptVz40c22EWGDFgTpIsRGzZVWP2QQtPCoz9KzMWTVVtqRa3KLDJSjIni'])
+client = OpenAI(api_key=os.environ['your-api-key'])
 
 # Function to generate a response from GPT-3
 def generate_healthwise_response(user_input):
